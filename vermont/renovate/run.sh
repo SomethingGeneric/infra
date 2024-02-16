22817ab0a92af3e90e754e1e05ebe1053f5fdec3
Matt C <matthew.compton@mymail.champlain.edu>
2024-02-16 17:09:31 -0500

    wahoo

--- vermont/renovate/run.sh
+++ vermont/renovate/run.sh
@@ -5,6 +5,8 @@ cd /home/matt/infra/vermont/renovate
 [[ ! -d venv ]] && python3 -m venv venv && venv/bin/pip install requests
 venv/bin/python3 get_repos.py
 
+echo "Updated list of repos"
+
 export PATH="/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
 export RENOVATE_CONFIG_FILE="/home/matt/renovate/config.js"
 
@@ -12,5 +14,7 @@ export LOG_LEVEL="debug"
 
 # hidden secret things (github token and forgejo token)
 [[ -f .variables.sh ]] && source .variables.sh
+
+echo "Spawning renovate"
 
 renovate
