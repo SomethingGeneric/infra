8d8022d581419b05ad742c99d0cf0d713d21e868
Matt C <matthew.compton@mymail.champlain.edu>
2024-02-16 16:32:36 -0500

    shell script updates

--- vermont/renovate/run.sh
+++ vermont/renovate/run.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+cd /home/matt/infra/vermont/renovate
+
 export PATH="/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
 export RENOVATE_CONFIG_FILE="/home/matt/renovate/config.js"
 
 export LOG_LEVEL="debug"
 
+# hidden secret things (github token and forgejo token)
 [[ -f .variables.sh ]] && source .variables.sh
 
 renovate
