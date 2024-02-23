b8e586f5d31305126067bd1cbb0a47a36115f8f8
Matt C <matthew.compton@mymail.champlain.edu>
2024-02-23 09:34:00 -0500

    bsky env

---
+++ vermont/pds/genenv.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Secure generator comands
+GENERATE_SECURE_SECRET_CMD="openssl rand --hex 16"
+GENERATE_K256_PRIVATE_KEY_CMD="openssl ecparam --name secp256k1 --genkey --noout --outform DER | tail --bytes=+8 | head --bytes=32 | xxd --plain --cols 32"
+
+PDS_DATADIR="/pds"
+PDS_DID_PLC_URL="https://plc.directory"
+PDS_BSKY_APP_VIEW_URL="https://api.bsky.app"
+PDS_BSKY_APP_VIEW_DID="did:web:api.bsky.app"
+PDS_REPORT_SERVICE_URL="https://mod.bsky.app"
+PDS_REPORT_SERVICE_DID="did:plc:ar7c4by46qjdydhdevvrndac"
+PDS_CRAWLERS="https://bsky.network"
+
+# Generate environment variables
+printf 'PDS hostname: '
+read PDS_HOSTNAME
+
+printf "Admin password: "
+read PDS_ADMIN_PASSWORD
+
+
+cat <<PDS_CONFIG >"${PDS_DATADIR}/pds.env"
+PDS_HOSTNAME=${PDS_HOSTNAME}
+PDS_JWT_SECRET=$(eval "${GENERATE_SECURE_SECRET_CMD}")
+PDS_ADMIN_PASSWORD=${PDS_ADMIN_PASSWORD}
+PDS_PLC_ROTATION_KEY_K256_PRIVATE_KEY_HEX=$(eval "${GENERATE_K256_PRIVATE_KEY_CMD}")
+PDS_DATA_DIRECTORY=${PDS_DATADIR}
+PDS_BLOBSTORE_DISK_LOCATION=${PDS_DATADIR}/blocks
+PDS_DID_PLC_URL=${PDS_DID_PLC_URL}
+PDS_BSKY_APP_VIEW_URL=${PDS_BSKY_APP_VIEW_URL}
+PDS_BSKY_APP_VIEW_DID=${PDS_BSKY_APP_VIEW_DID}
+PDS_REPORT_SERVICE_URL=${PDS_REPORT_SERVICE_URL}
+PDS_REPORT_SERVICE_DID=${PDS_REPORT_SERVICE_DID}
+PDS_CRAWLERS=${PDS_CRAWLERS}
+LOG_ENABLED=true
+PDS_CONFIG
